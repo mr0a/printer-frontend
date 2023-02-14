1133f675217150b8fc13e8d42d56f501cba756f3
Aravindhan <aravindhan.ct19@bitsathy.ac.in>
2023-02-14 07:24:21 +0530

    edited error page

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,11 +1,12 @@
 import { useRouteError } from "react-router-dom";
-
+import "./404-error.css";
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
     <div id="error-page">
+      <img src="./404-error.svg" alt="Oops" width="500rem"></img>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
